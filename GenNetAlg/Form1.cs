641df82ebb86e60d03706a56a8aa286bac953da7
ReiReiRei <frostball@gmail.com>
2014-03-26 01:31:28 +0400

    init

---
+++ GenNetAlg/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenNetAlg
+{
+    public partial class Form1 : Form
+    {
+
+        int networkSize = 10;
+        int populationSize = 30;
+        int firstPoint = 0;
+        int lastPoint = 3;
+        int numberOfIterations = 50000;
+        RoutingTable Tb;
+        Population Po;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            
+
+
+          
+
+            for (int i = 0; i < numberOfIterations; i++)
+            {
+                int cur_size = Po.size * 2;
+                for (int j = 0; j < cur_size; j++)
+                {
+                    Po.Chromosomes.Add(Chromosome.Cross(Po.Random, Po.Random).Mutate());
+                }
+
+                
+
+                //foreach (var cr in Po.Chromosomes)
+                //{
+                //    if (cr.isNew == false)
+                //    {
+                //        Po.Chromosomes.Remove(cr);
+                //    }
+
+                //}
+
+                foreach (var cr in Po.Chromosomes)
+                {
+                cr.isNew = false;
+                }
+
+
+
+
+
+                Po.Chromosomes.Sort(Chromosome.Compr);
+                if (populationSize > Po.size)
+                {
+                    Po.Chromosomes = Po.Chromosomes.GetRange(0, Po.size);
+                }
+                else
+                {
+                    Po.Chromosomes = Po.Chromosomes.GetRange(0, populationSize - 1);
+                }
+                Po.Terminate();
+                Populate.Text = Po.ToString();
+
+             
+
+
+
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           Tb = new RoutingTable(networkSize);
+           Po = new Population(populationSize, networkSize, firstPoint, lastPoint, Tb);
+
+           Populate.Text = Po.ToString();
+           Route.Text = Tb.ToString();
+        }
+    }
+}
---
+++ GenNetAlg/RoutingTable.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenNetAlg
+{
+    class RoutingTable
+    {
+        public int[,] Table
+        {
+            get;
+            set;
+        }
+        private int[] weight_set = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100001 };
+
+
+        public RoutingTable(int size)
+        {
+            Table = new int[size, size];
+            Random rnd = new Random();
+
+            for (int i = 0; i < Table.GetLength(0); i++)
+            {
+                for (int j = 0; j < Table.GetLength(1); j++)
+                {
+                    if (i != j)
+                    {
+                        Table[i, j] = weight_set[rnd.Next(0, weight_set.Length-1)];
+                    }
+                    else
+                    {
+                        Table[i, j] = 0;
+                    }
+                }
+            }
+
+        }
+
+        private RoutingTable()
+        { }
+
+        public override string ToString()
+        {
+            string s = "";
+            for(int i=0;i<Table.GetLength(0);i++)
+            {  
+                for(int j=0;j<Table.GetLength(1);j++)
+                {
+                    s = s + Table[i, j].ToString() + " ";
+                }
+                s = s + "\r\n";
+            }
+            return s;
+            
+
+        }
+
+
+
+    }
+}
